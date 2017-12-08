4ab9b725216f620f15c947c10a5a0d6d8ccb6051
ChristianCme <cmehil.warn@gmail.com>
2017-12-08 01:42:39 -0600

    Add partial endpoints

--- server.js
+++ server.js
@@ -4,9 +4,12 @@ var nconf         = require('nconf')
 var fs            = require('fs')
 var chalk         = require('chalk')
 var passport      = require('passport')
-var LocalStrategy = require('passport-local').Strategy;
+var LocalStrategy = require('passport-local').Strategy
 var session       = require("express-session")
 var bodyParser    = require("body-parser");
+var books         = require('google-books-search');
+var mysql         = require('mysql')
+
 
 // Local requires
 var db = require('./db');
@@ -26,6 +29,7 @@ nconf
       host: '127.0.0.1',
       port: '3306',
       user: 'root',
+      password: 'root';
       name: 'bookswap'
     },
     server: {
@@ -34,6 +38,15 @@ nconf
     }
   })
 
+//Connect to database
+var connection = mysql.createConnection({
+  host     : nconf.host,
+  user     : nconf.user,
+  password : nconf.password,
+  database : nconf.name
+});
+
+connection.connect()
 
 
 // Setup Passport.js
@@ -92,6 +105,43 @@ app.get('*', (req, res) => {
     res.sendFile('public/index.html')
   }
 });
+
+app.get('/search_new', (req, res) => {
+    books.search(res.params.q, function(error, results) {
+      if (!error) {
+          req.send(results)
+      } else {
+          console.log(error);
+      }
+  })
+})
+
+app.get('/user_inv' (req, res) => {
+  var user_id = ????
+  connection.query('SELECT author,title FROM book,inventory WHERE user_id =' + user_id + 'AND isbn = book_isbn;', function(error, rows) {
+        res.send( rows.map(row => row.query_text)
+        })
+    })
+})
+
+app.get('/user_wishlist' (req, res) => {
+  var user_id = ????
+  connection.query('SELECT author,title FROM book,wishlist WHERE user_id =' + user_id + 'AND isbn = book_isbn;', function(error, rows) {
+        res.send( rows.map(row => row.query_text)
+        })
+    })
+})
+
+app.get('/remove_inv' (req, res) => {
+  var user_id = ????
+  var title = req.title
+  connection.query('DELETE FROM inventory JOIN book ON isbn=book_isbn WHERE book.title =' + title + ' AND user_id =' + user_id + ' LIMIT 1;', function(error, rows) {
+      res.send( rows.map(row => row.query_text)
+      })
+  })
+})
+
+
 
 app.listen(nconf.get('server:port'), () => {
   console.log(chalk.green('bookswap started on port ' + chalk.bold(nconf.get('server:port'))))
